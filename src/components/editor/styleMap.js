fbfb9729b29546f8b330aed5fc938e978e294595
Milo Fultz <milopfultz@gmail.com>
2021-04-30 20:49:48 -0700

    Add strikethrough

--- src/components/editor/styleMap.js
+++ src/components/editor/styleMap.js
@@ -4,10 +4,10 @@ const styleMap = {
         regEx: /\*{2}(?<text>.*?)\*{2}/g,
         keyCharsCount: 2 //This is the number of characters in the key, 1 for * and 2 for **
     },
-    'MYSTYLE2': {
+    'strikethrough': {
         textDecoration: 'line-through',
-        regEx: /\-(?<text>.*?)\-/g,
-        keyCharsCount: 1
+        regEx: /~{2}(?<text>.*?)~{2}/g,
+        keyCharsCount: 2
     },
     'MYSTYLE3': {
         color: 'red',
