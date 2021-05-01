d0ac060eac841eb256226a7fe548725c7b6b2144
Milo Fultz <milopfultz@gmail.com>
2021-04-30 20:52:06 -0700

    Add italics

--- src/components/editor/styleMap.js
+++ src/components/editor/styleMap.js
@@ -4,15 +4,15 @@ const styleMap = {
         regEx: /\*{2}(?<text>.*?)\*{2}/g,
         keyCharsCount: 2 //This is the number of characters in the key, 1 for * and 2 for **
     },
+    'italic': {
+        fontStyle: 'italic',
+        regEx: /\*(?<text>.*?)\*/g,
+        keyCharsCount: 1
+    },
     'strikethrough': {
         textDecoration: 'line-through',
         regEx: /~{2}(?<text>.*?)~{2}/g,
         keyCharsCount: 2
-    },
-    'MYSTYLE3': {
-        color: 'red',
-        regEx: /\+\+\+(?<text>.*?)\+\+\+/g,
-        keyCharsCount: 3
     },
 };
 
