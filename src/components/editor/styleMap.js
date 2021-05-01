f13ab7b2db6de88201fbfd4a173a21ab783daac2
Milo Fultz <milopfultz@gmail.com>
2021-04-30 20:59:15 -0700

    Make bold regex accept underscores

--- src/components/editor/styleMap.js
+++ src/components/editor/styleMap.js
@@ -1,7 +1,7 @@
 const styleMap = {
     'bold': {
         fontWeight: 'bold',
-        regEx: /\*{2}(?<text>.*?)\*{2}/g,
+        regEx: /((\*{2})|(_{2}))(?<text>.*?)\1/g,
         keyCharsCount: 2 //This is the number of characters in the key, 1 for * and 2 for **
     },
     'italic': {
