8e07c5f29ad3a7cf637dcc0c43e129b433fa993d
Milo Fultz <milo.fultz@brandlive.com>
2021-04-30 17:43:15 -0700

    Make regex matches in group instead of capture ref
    This allows for more readability in the code and more freedom later to
    use capture groups, lookaheads, lookbehinds for more precise matching.

--- src/components/editor/styleMap.js
+++ src/components/editor/styleMap.js
@@ -1,17 +1,17 @@
 const styleMap = {
     'bold': {
         fontWeight: 'bold',
-        regEx: /\*{2}(.*?)\*{2}/g,
+        regEx: /\*{2}(?<text>.*?)\*{2}/g,
         keyCharsCount: 2 //This is the number of characters in the key, 1 for * and 2 for **
     },
     'MYSTYLE2': {
         textDecoration: 'line-through',
-        regEx: /\-(.*?)\-/g,
+        regEx: /\-(?<text>.*?)\-/g,
         keyCharsCount: 1
     },
     'MYSTYLE3': {
         color: 'red',
-        regEx: /\+\+\+(.*?)\+\+\+/g,
+        regEx: /\+\+\+(?<text>.*?)\+\+\+/g,
         keyCharsCount: 3
     },
 };
