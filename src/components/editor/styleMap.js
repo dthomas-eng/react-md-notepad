c2d5a09ebe4c9e47deed29b5792e0ac579569277
Milo Fultz <milopfultz@gmail.com>
2021-04-30 21:01:27 -0700

    Ensure regex only triggers if chars are within
    I noticed that when italic was set up, bold no longer worked. This was
    because double underscore was being interpreted as a single italic group
    without any inner chars. This will stop those false positives from
    happening.

--- src/components/editor/styleMap.js
+++ src/components/editor/styleMap.js
@@ -1,17 +1,17 @@
 const styleMap = {
     'bold': {
         fontWeight: 'bold',
-        regEx: /((\*{2})|(_{2}))(?<text>.*?)\1/g,
+        regEx: /((\*{2})|(_{2}))(?<text>.+?)\1/g,
         keyCharsCount: 2 //This is the number of characters in the key, 1 for * and 2 for **
     },
     'italic': {
         fontStyle: 'italic',
-        regEx: /\*(?<text>.*?)\*/g,
+        regEx: /(\*|_)(?<text>.+?)\1/g,
         keyCharsCount: 1
     },
     'strikethrough': {
         textDecoration: 'line-through',
-        regEx: /~{2}(?<text>.*?)~{2}/g,
+        regEx: /~{2}(?<text>.+?)~{2}/g,
         keyCharsCount: 2
     },
 };
