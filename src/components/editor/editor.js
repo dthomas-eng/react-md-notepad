01e744a26a32c961de3b6584ab7b01a7ca9f2783
Derick <derick.thomas.me@gmail.com>
2021-05-02 08:29:56 -0700

    Added image drag and drop
    Added the ability for the user to drag any image media (PNG, JPEG, BMP,
    ect...) into the editor area.
    
    One issue I was having in doing this was that if you drop directly into
    the draftjs DOM element (by giving it an event listener and detecting a
    drop), draftjs breaks. Not sure what the root cause is, but the blocks
    in the contentState stop updating after the drop and no markdown is
    rendered. To get around this, I added a textArea that pops up only when
    something is dragged into the editor area. That way, the listener is not
    attached to the draftjs editor and everything keeps working.
    
    Current known issues:
    
    	- Occasionally, entering a new line of text will copy the image.
    	  Have not yet looked into why this happens.
    	- A drag without a drop results in the editor getting hidden
    	  behind a transparent textArea. Need to add logic to determine
    	  if the drag was resolved (dropped or dragged out of the
    	  editor) to fix this. Should be simple.

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -4,7 +4,9 @@ import {
   EditorState,
   convertToRaw,
   RichUtils,
-  SelectionState
+  SelectionState,
+  AtomicBlockUtils,
+  EditorBlock
 } from "draft-js";
 import Editor from "draft-js-plugins-editor";
 
@@ -27,6 +29,12 @@ class PageContainer extends React.Component {
       matchStrings: Object.values(styleMap).map((prop) => prop.regEx),
       styles: Object.keys(styleMap).map((prop) => prop),
     });
+
+    //Add listeners for dragging over and dropping in the editor area. 
+    let div = this.dropRef.current
+    let editorDiv = document.getElementById('editor')
+    editorDiv.addEventListener('dragover', this.handleDrag)
+    div.addEventListener('drop', this.handleDrop)
   };
 
   //Whevener anything changes, the state gets updated.
@@ -35,6 +43,9 @@ class PageContainer extends React.Component {
       editorState
     });
   };
+
+  //A ref that points to the editor area to make it droppable. Could (and probably should) just select this id.
+  dropRef = React.createRef()
 
   //Two key presses are listed for: 
   //Enter - when enter is pressed, the whole doc is scanned for our regexes and rendered.
@@ -47,6 +58,65 @@ class PageContainer extends React.Component {
       this.clearStyles()
     }
   }
+
+  /** Handler Methods **/
+
+  //This method turns a tab into 4 spaces. 
+  handleTab = (e) => {
+    e.preventDefault();
+
+    let currentState = this.state.editorState;
+    let newContentState = Modifier.replaceText(
+      currentState.getCurrentContent(),
+      currentState.getSelection(),
+      "    "
+    );
+
+    this.setState({
+      editorState: EditorState.push(currentState, newContentState, 'insert-characters')
+    });
+  }
+
+  //When anything is dragged over the editor, we turn on a text area that actually accepts the media. If something is
+  //dropped directly into the draft editor, everything breaks.
+  handleDrag = (e) => {
+
+    document.getElementById("dropzone").style.width = `${document.getElementById("editor").offsetWidth}px`
+    document.getElementById("dropzone").style.height = `${document.getElementById("editor").offsetHeight}px`;
+    document.getElementById('dropzone').style.visibility = 'visible'
+
+  }
+
+  //When media is dropped into the editor area, this is called.
+  handleDrop = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    //Only allow 1 file to be dropped. 
+    if (e.dataTransfer.files && e.dataTransfer.files.length === 1) {
+
+      //And that file must be an image. Can detect different file types and treat accordingly.
+      if (!e.dataTransfer.files[0].type.includes('image')) {
+        alert('File type not supported (yet).')
+        return
+      }
+
+      const sendToInsertBlock = (src) => this.insertBlock(src)
+      let reader = new FileReader()
+      reader.readAsDataURL(e.dataTransfer.files[0])
+
+      reader.onloadend = function () {
+        sendToInsertBlock(reader.result)
+        //Need to hide the textArea that shows up when you hover to drop. It's done it's job at this point. 
+        document.getElementById('dropzone').style.visibility = 'hidden'
+      }
+
+    } else if (e.dataTransfer.files.length > 1) {
+      alert('Only 1 file at a time (for now)')
+    }
+  }
+
+  /** Markdown Parsing Methods **/
 
   //This method scans, block by block, for any matching regexes and replaces tag chars with style. 
   renderEverything = () => {
@@ -86,7 +156,7 @@ class PageContainer extends React.Component {
           if (match) {
             // Extract text content
             let textContent = match.groups.text;
-             // currently is set up so match group 1 is always first half of symmetrical selector
+            // currently is set up so match group 1 is always first half of symmetrical selector
             let tagContent = match[1];
 
             //Get start and end of the match. This is relative to the text block.
@@ -200,27 +270,75 @@ class PageContainer extends React.Component {
     }
   }
 
-  //This method turns a tab into 4 spaces. 
-  handleTab = (e) => {
-    e.preventDefault();
+  /** Media Drag/Drop Methods **/
 
-    let currentState = this.state.editorState;
-    let newContentState = Modifier.replaceText(
-      currentState.getCurrentContent(),
-      currentState.getSelection(),
-      "    "
+  //Inserts a block when an image is dragged into the drag and drop zone. 
+  insertBlock = (source) => {
+
+    const { editorState } = this.state;
+    const contentState = editorState.getCurrentContent();
+    const contentStateWithEntity = contentState.createEntity(
+      "MEDIA",
+      "MUTABLE",
+      { mediaSrc: source }
     );
 
-    this.setState({
-      editorState: EditorState.push(currentState, newContentState, 'insert-characters')
+    const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+    const newEditorState = EditorState.set(editorState, {
+      currentContent: contentStateWithEntity
     });
+
+    this.setState({ ...this.state, imgsrc: source }, () => {
+
+      this.setState({
+        editorState: AtomicBlockUtils.insertAtomicBlock(
+          newEditorState,
+          entityKey,
+          " "
+        )
+      })
+    })
   }
 
+  //This is the method that renders each block. When we define custom blocks (like the ImageBlock below), 
+  //we can define what data goes where. In this case, we take the data from the contentstate (which is the imgsrc)
+  //and put it into the props of the custom ImageBlock.
+  //Could detect type of media (pdf, audio, video, ect...) and apply the same routine displaying them however we want.
+  blockRenderer = contentBlock => {
+    const type = contentBlock.getType();
+
+    if (type === "atomic") {
+
+      //get the data from that block: 
+      const { editorState } = this.state;
+      const contentState = editorState.getCurrentContent();
+      const data = contentState.getEntity(contentBlock.getEntityAt(0)).getData();
+
+      return {
+        component: this.ImageBlock,
+        editable: true,
+        props: {
+          imgsrc: data.mediaSrc
+        }
+      };
+    }
+  };
+
+    //The actual definition of the custom block. 
+    ImageBlock = props => {
+      return (
+        <div id={'imageLoc'}>
+          <img src={props.blockProps.imgsrc} id={'imageBlock'} />
+        </div>
+      );
+    };
+
+    
   render() {
     return (
       <Fragment >
-        {/* Maybe give it a tasty toolbar or logo in this padding above the editor?  */}
-        <div className="editor" onKeyDown={this.handleKeyDown}>
+        <textarea ref={this.dropRef} id='dropzone' style={{ border: '1px solid blue', position: 'fixed', top: '0', left: '0', opacity: '.4', visibility: 'hidden', color: 'transparent' }} />
+        <div className='editor' id='editor' onKeyDown={this.handleKeyDown}>
           <Editor
             editorState={this.state.editorState}
             handleKeyCommand={this.handleKeyCommand}
